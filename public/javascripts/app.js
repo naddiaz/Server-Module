5a2681c7e92d459a68bd7b6de32e01d176ea918e
Néstor Álvarez Díaz <nad.9205@gmail.com>
2015-01-29 13:48:11 +0000

    Mapa con AngularJS

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,16 @@
 (function(){
-  var app = angular.module('store',[]);
+  var app = angular.module('aMap', ['ngMap']);
+ 
+  app.controller('MapCtrl', function (){
+                  
+        this.center = {
+          latitude: 28,
+          longitude: -16
+        };
+        this.zoom = 6;
+
+      this.location = function(){
+        return this.center.latitude + "," + this.center.longitude;
+      }
+  });
 })();
