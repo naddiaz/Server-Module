c2279727258b08513019c7c07de8d2f3acfb256c
Néstor Álvarez Díaz <nad.9205@gmail.com>
2015-02-02 21:10:34 +0000

    Simple Date WS

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,16 @@
 exports.index = function(req, res){
   res.render('index', { title: 'BLE Task: MAPA'});
 };
+
+exports.ws = function(req, res){
+  var currentTime = new Date();
+  data = {
+    second: currentTime.getSeconds(),
+    minute: currentTime.getMinutes(),
+    hour: currentTime.getHours(),
+    day: currentTime.getDay()+1,
+    month: currentTime.getMonth()+1,
+    year: currentTime.getFullYear()
+  };
+  res.json(data);
+};
