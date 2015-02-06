fc605303bfe6af5ce10e1423933e7c7b92899ac8
Néstor Álvarez Díaz <nad.9205@gmail.com>
2015-02-06 17:49:02 +0000

    Definicion de los modelos

--- dbmodels.js
+++ dbmodels.js
@@ -10,6 +10,45 @@ var Person = new Schema({
   device_type: {type: String, default: 'android'}
 });
 
+var Localization = new Schema({
+  id_person: Number,
+  id_beacon: Number,
+  date: Date
+});
+
+var Beacon = new Schema({
+  id_beacon: Number,
+  position: [{
+    x: Number,
+    y: Number
+  }]
+});
+
+var Task = new Schema({
+  id_task: {type: Number, unique: true},
+  id_beacon: Number,
+  type: String,
+  priority: {type: Number, enum: [0,1,2,3,4]},
+  description: String
+});
+
+var Work = new Schema({
+  id_person: Number,
+  id_task: Number,
+  state: {type: String, enum: ["no_asign", "asign", "running", "finish", "pause", "cancel"]},
+  date: [{
+    created_at: Date,
+    asign_at: Date,
+    running_at: Date,
+    finish_at: Date,
+    pause_at: Date,
+    cancel_at: Date
+  }]
+});
 
 var db = Mongoose.connect('mongodb://localhost/bletaskerDB');
 db.model('Person', Person);
+db.model('Localization', Localization);
+db.model('Beacon', Beacon);
+db.model('Task', Task);
+db.model('Work', Work);
