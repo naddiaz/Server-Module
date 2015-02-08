e392918eaf4388dd15216923b52a5c5707cebb85
Néstor Álvarez Díaz <nad.9205@gmail.com>
2015-02-08 10:58:23 +0000

    De polygons a circles en el mapa de beacons

--- public/javascripts/maps/TF_north_airport.js
+++ public/javascripts/maps/TF_north_airport.js
@@ -9,15 +9,33 @@ function initialize() {
     cells: [
       {
           cell: "0",
-          path: [[28.488214,-16.347497],[28.487897,-16.347632],[28.487612,-16.346793],[28.487918,-16.34666]]
+          path: [28.488213,-16.347496],
+          color: '#F44336'
       },
       {
           cell: "1",
-          path: [[28.487797,-16.347412],[28.487574,-16.346735],[28.487379,-16.346836],[28.487579,-16.347367],[28.487645,-16.347344]]
+          path: [28.487901,-16.347633],
+          color: '#9C27B0'
       },
       {
           cell: "2",
-          path: [[28.487749,-16.346565],[28.488029,-16.346302],[28.488107,-16.346551]]
+          path: [28.48797,-16.347287],
+          color: '#3F51B5'
+      },
+      {
+          cell: "3",
+          path: [28.487675,-16.347359],
+          color: '#8BC34A'
+      },
+      {
+          cell: "4",
+          path: [28.488017,-16.346965],
+          color: '#FF9800'
+      },
+      {
+          cell: "5",
+          path: [28.487741,-16.347045],
+          color: '#69F0AE'
       }
     ]
   };
@@ -31,16 +49,15 @@ function initialize() {
       mapOptions);
 
   for(i in airport.cells){
-    paths_cell = [];
-    for(j in airport.cells[i].path){
-      paths_cell.push(new google.maps.LatLng(airport.cells[i].path[j][0], airport.cells[i].path[j][1]));
-    }
-    cell = new google.maps.Polygon({
-      paths: paths_cell,
+    paths_cell = new google.maps.LatLng(airport.cells[i].path[0], airport.cells[i].path[1]);
+    
+    cell = new google.maps.Circle({
+      center: paths_cell,
+      radius: 20,
       strokeColor: '#FF0000',
       strokeOpacity: 0.8,
       strokeWeight: 2,
-      fillColor: '#FF0000',
+      fillColor: airport.cells[i].color,
       fillOpacity: 0.35,
       indexID: "cell_" + i
     });
@@ -53,4 +70,10 @@ function initialize() {
     });
   }
 }
+
+function randColor(){
+  // #CCCCCC to #222222
+  return Math.floor(Math.random() * (13421772 - 2236962) + 2236962).toString(16);
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
