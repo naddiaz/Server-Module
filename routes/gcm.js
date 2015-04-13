4b6c700202e96ce70835693758c2163e34d53fee
Néstor Álvarez Díaz <nad.9205@gmail.com>
2015-04-13 23:05:35 +0100

    Cambios en el envio del mensaje gcm

--- routes/gcm.js
+++ routes/gcm.js
@@ -36,37 +36,38 @@ exports.registrationGCM =  function(req, res){
 };
 
 exports.create =  function(req, res){
-  //Airport.findOne({location: req.body.location, name: req.body.name }).select('id_airport').exec(function(err, airport){
 
-    var message = new gcm.Message();
-    message.addData('description',req.body.id_task + "," + req.body.description);
+  var Parameters = Parameter.findOne({name: "api_key"});
+  var GCMs = GCM.find({id_airport: req.body.id_airport, id_person: req.body.id_person});
+  var Tasks = Task.findOne({id_airport: req.body.id_airport,id_task: req.body.id_task});
 
-    var Parameters = Parameter.findOne({name: "api_key"});
-    var GCMs = GCM.find({id_airport: req.body.id_airport, id_person: req.body.id_person});
+  var data = {
+    parameters: Parameters.exec.bind(Parameters),
+    gcms: GCMs.exec.bind(GCMs),
+    tasks: Tasks.exec.bind(Tasks)
+  };
+  async.parallel(data,function(err,results){
+    if(err)
+      res.send({status: err});
+    else{
+      var message = new gcm.Message();
+      console.log
+      message.addData('description',results.tasks[0].id_task + "," + results.tasks[0].description);
 
-    var data = {
-      parameters: Parameters.exec.bind(Parameters),
-      gcms: GCMs.exec.bind(GCMs)
-    };
-    async.parallel(data,function(err,results){
-      if(err)
-        res.send({status: err});
-      else{
-        var sender = new gcm.Sender(results.parameters.value);
-        var regIds = [];
-        for(i in results.gcms){
-          regIds.push(results.gcms[i].id_push);
-        }
-        sender.send(message, regIds, function (err, result) {
-          if(err){
-            console.log(err);
-            res.send(err);
-          }
-          res.send({status: true});
-        });
+      var sender = new gcm.Sender(results.parameters.value);
+      var regIds = [];
+      for(i in results.gcms){
+        regIds.push(results.gcms[i].id_push);
       }
-    });
-  //});
+      sender.send(message, regIds, function (err, result) {
+        if(err){
+          console.log(err);
+          res.send(err);
+        }
+        res.send({status: true});
+      });
+    }
+  });
 };
 
 exports.sendNotificationTest =  function(req, res){
