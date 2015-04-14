68d4f87fa93d099e58c55088556646a3b7eefc87
Néstor Álvarez Díaz <nad.9205@gmail.com>
2015-04-14 19:57:44 +0100

    collapseKey GCM

--- routes/gcm.js
+++ routes/gcm.js
@@ -66,7 +66,7 @@ exports.create =  function(req, res){
         id_task: results.tasks.id_task,
         description: results.tasks.description
       })
-      //message.collapseKey = 'taskPush';
+      message.collapseKey = 'taskPush';
 
       var sender = new gcm.Sender(results.parameters.value);
 
