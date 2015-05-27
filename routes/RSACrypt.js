d5d44a6f62e763173780f7ec4b4cdc8c485f2cf8
Néstor Álvarez Díaz <naddiaz.92@gmail.com>
2015-05-27 10:45:00 +0100

    Cambio de decrypt a funciones del sistema con exec

--- routes/RSACrypt.js
+++ routes/RSACrypt.js
@@ -20,39 +20,48 @@ exports.sign = function(text){
 }
 
 exports.decrypt = function(text){
-      //var key = new NodeRSA();
-      var buf = new Buffer(text, 'base64');
-      console.log(buf);
-      var privateKey = '/server/server.private.pem';
-      //key.importKey(fs.readFileSync(path.join(__dirname, '../cert') + privateKey),'pkcs8');
-      var pem = fs.readFileSync(path.join(__dirname, '../cert') + privateKey);
-      //var key = new NodeRSA(pem);
-      //return key.decrypt(buf);
-
-var exec = require('exec');
-
-fs.writeFileSync("data.bin",buf);
-fs.writeFileSync("key.pem",pem);
-
-    exec(['openssl', 'pkeyutl', '-decrypt', '-in', 'data.bin', '-inkey', 'key.pem', '-out', 'response.json'], function(err, out, code) {
-        if (err instanceof Error)
-          throw err;
-        process.stderr.write(err);
-        process.stdout.write(out);
-	console.log("TERMINAR EL EXEC");
-//    });
-var json = fs.readFileSync("response.json");
-console.log("JSON" + json);
-exec(['rm','-rf','data.bin','key.pem','response.json'],function(err,out,code){
-   if(err instanceof Error)
+  //Base 64 to Buffer encrypted text
+  var exec = require('exec');
+  var buffer = new Buffer(text, 'base64');
+  //Read private server key
+  var privateKey = fs.readFileSync(path.join(__dirname, '../cert') + '/server/server.private.pem');
+  var dirname = "d" + random();
+  //Creamos el subdirectorio (dirname) dentro de temp
+  var json = null;
+  exec(['mkdir','temp/' + dirname],function(err,out,code){
+    if(err instanceof Error){
       throw err;
-   process.stderr.write(err);
-   process.stdout.write(out);
-   console.log("ELIMINAR ARCHIVOS");
-});
-console.log(json.toString());
-});
-      return "TEST";
+    }
+    else{
+      //Creamos un archivo que contiene los datos del buffer
+      fs.writeFileSync(path.join(__dirname, '../temp/') + dirname + "/data.bin",buffer);
+      //Con la clave y los datos en ficheros ejecutamos openssl
+      exec(['openssl', 'pkeyutl',
+        '-decrypt', '-in', 'temp/' + dirname + '/data.bin',
+        '-inkey', 'cert/server/server.private.pem',
+        '-out', 'temp/' + dirname + '/response.json'],
+      function(err, out, code) {
+        if(err instanceof Error){
+          throw err;
+        }
+        else{
+          //Guardamos la aplicacion en la variable json
+          json = fs.readFileSync('temp/' + dirname + '/response.json');
+          //Eliminamos el directorio temporal
+          exec(['rm','-rf','temp/' + dirname],function(err,out,code){
+             if(err instanceof Error)
+                throw err;
+             process.stderr.write(err);
+             process.stdout.write(out);
+          });
+        }
+      });
+    }
+  });
+  if(json != null)
+    return json;
+  else
+    return "TEST";
 }
 
 exports.verify = function(text,signature,airport,employee){
@@ -60,4 +69,10 @@ exports.verify = function(text,signature,airport,employee){
       var publicKey = '/employees/a' + airport + 'e' + employee + '/a' + airport + 'e' + employee + '.public.der';
       key.importKey(fs.readFileSync(path.join(__dirname, '../cert') + publicKey),'pkcs8-public-der');
       return key.verify(text,signature,"UTF-8","base64");
+}
+
+function random () {
+  var low = Math.random();
+  var high = Math.random() + low;
+  return Math.random() * (high - low) + low;
 }
